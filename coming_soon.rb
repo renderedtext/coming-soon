f1a59c9039c6914b23b8917a359aa473294a59e4
Marko Anastasov <marko@renderedtext.com>
2010-12-13 12:18:14 +0100

    Make application name configurable.

--- coming_soon.rb
+++ coming_soon.rb
@@ -1,7 +1,16 @@
 require 'sinatra/base'
 
 class ComingSoon < Sinatra::Base
+
+  configure do
+    if !File.exist?('config.yml')
+      puts "There's no configuration file at config.yml!"
+      exit!
+    end
+    APP_CONFIG = YAML.load_file('config.yml')
+  end
+
   get '/' do
-    'Hello ComingSoon!'
+    "Hello #{APP_CONFIG['app_name']}!"
   end
 end
